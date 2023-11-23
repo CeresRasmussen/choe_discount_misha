c8886bd908d15f2c91e5dde18103c16a0887503f
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-11-23 23:40:35 +0200

    almost done

---
+++ src/index.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const animatedBlocks = document.querySelectorAll('.animated');
+
+  function isElementInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function handleScroll() {
+    animatedBlocks.forEach(function (animatedBlock) {
+      if (isElementInViewport(animatedBlock)) {
+        console.log('animatedBlock:', animatedBlock);
+        animatedBlock.classList.add('animate');
+      }
+    });
+  }
+  document.addEventListener('scroll', handleScroll);
+});
