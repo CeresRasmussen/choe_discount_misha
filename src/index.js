180429b6b33bb956855f324a0d01127d446fbef3
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-11-30 22:08:16 +0200

    add button-up and some fix
    have been trying horizontal scroll and comment it. need more elements

--- src/index.js
+++ src/index.js
@@ -21,3 +21,48 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   document.addEventListener('scroll', handleScroll);
 });
+
+// add up button
+
+const btnUp = {
+  el: document.querySelector('.btn-up'),
+  scrolling: false,
+  show() {
+    if (this.el.classList.contains('btn-up_hide')) {
+      this.el.classList.remove('btn-up_hide');
+    }
+  },
+  hide() {
+    if (!this.el.classList.contains('btn-up_hide')) {
+      this.el.classList.add('btn-up_hide');
+    }
+  },
+  addEventListener() {
+    window.addEventListener('scroll', () => {
+      const scrollY = window.scrollY || document.documentElement.scrollTop;
+      if (this.scrolling && scrollY > 0) {
+        return;
+      }
+      this.scrolling = false;
+
+      if (scrollY > 300) {
+        this.show();
+      } else {
+        this.hide();
+      }
+    });
+
+    document.querySelector('.btn-up').onclick = () => {
+      this.scrolling = true;
+      this.hide();
+
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    };
+  },
+};
+
+const result = btnUp.addEventListener();
