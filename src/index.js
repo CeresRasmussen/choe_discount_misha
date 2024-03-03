f0e283bfd0f93277c05ce4f22408a0745ca8a64f
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2024-03-03 19:01:08 +0200

    add modal windiw for the comment

--- src/index.js
+++ src/index.js
@@ -122,3 +122,49 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 2000); // Через 2 секунди прибрати повідомлення
   }
 });
+
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector('[data-modal-open]'),
+//     closeModalBtn: document.querySelector('[data-modal-close]'),
+//     modal: document.querySelector('[data-modal]'),
+//     backdrop: document.querySelector('.backdrop'),
+//   };
+
+//   refs.openModalBtn.addEventListener('click', toggleModal);
+//   refs.closeModalBtn.addEventListener('click', toggleModal);
+//   refs.backdrop.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.modal.classList.toggle('is-hidden');
+//     document.body.classList.toggle('modal-open');
+//   }
+// })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    backdrop: document.querySelector('.backdrop'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.backdrop.addEventListener('click', handleBackdropClick);
+  refs.modal.addEventListener('click', handleModalClick);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    document.body.classList.toggle('modal-open');
+  }
+
+  function handleBackdropClick(event) {
+    if (event.target === event.currentTarget) {
+      toggleModal();
+    }
+  }
+
+  function handleModalClick(event) {
+    event.stopPropagation(); // Зупинка подальшого розповсюдження події
+  }
+})();
